2bd04a686f5bec1f85859da44bcaf3616d0528fd
liuyuanfa <463001681@qq.com>
2023-08-31 14:26:52 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/reset.css";
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(Antd).mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
   </div>
+  <a-button type="primary">测试AntV按钮</a-button>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<script></script>
